8a2d86709a2468e09396506e02dd3608df0598c3
Dominique Marcadet <dominique.marcadet@centralesupelec.fr>
2022-04-27 14:33:23 +0200

    add a remove option
    and use cp -R instead of mv -u (unknown on macOS)

--- scripts/update_ocl.sh
+++ scripts/update_ocl.sh
@@ -7,16 +7,16 @@ SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
 usage() {
     printf "Usage: $0 [OPTION...]\n\n"
     printf "Updates OCL validation files from the given branch by downloading them to the desired target directory.\n"
-    printf "Files will only be overwritten if they were updated since the last download.\n\n"
     printf "Options:\n"
     printf -- "-h --help        \tShow this help message\n"
+    printf -- "-r --remove      \tRemove previous versions of OCL files.\n"
     printf -- "-b --branch      \tThe branch of the OCL git repository to pull validation files from.\n"
     printf                 "\t\t\tDefault is 'master'.\n"
     printf -- "-o --out-dir     \tThe directory in which you want to store OCL files.\n"
     printf                 "\t\t\tDefault is '<ROOT>/ocl' where <ROOT> is the root of the testing repository.\n"
 }
 
-OPTS=$(getopt -o hb:o: -l help,branch:,out-dir: -- "$@")
+OPTS=$(getopt -o hrb:o: -l help,remove,branch:,out-dir: -- "$@")
 
 if [ $? -ne 0 ]; then
     echo "Options parsing failed, exiting..."
@@ -25,11 +25,16 @@ fi
 
 eval set -- "$OPTS"
 
+REMOVE=0
+
 while true; do
     case "$1" in
         -h | --help)
             usage
             exit 0;;
+        -r | --remove)
+            REMOVE=1
+            ;;
         -b | --branch)
             arg_required "$1" "$2"
             OCL_BRANCH="$2"
@@ -62,6 +67,10 @@ if [ -z "$OCL_OUTPUT_DIR" ]; then
     OCL_OUTPUT_DIR="$ROOT_DIR/ocl"
 fi
 
+if [ "$REMOVE" -eq 1 ]; then
+    rm -r "$OCL_OUTPUT_DIR"/*
+fi
+
 mkdir -p "$OCL_OUTPUT_DIR"
 
 TMP_DIR="$ROOT_DIR/tmp"
@@ -72,8 +81,8 @@ echo "Done."
 
 OCL_FILES="$TMP_DIR/fr.centralesupelec.edf.riseclipse.iec61850.scl.ocl/*"
 
-echo -n "Moving OCL files to '$OCL_OUTPUT_DIR'... "
-mv -u $OCL_FILES "$OCL_OUTPUT_DIR"
+echo -n "Copying OCL files to '$OCL_OUTPUT_DIR'... "
+cp -R $OCL_FILES "$OCL_OUTPUT_DIR"
 rm -rf "$TMP_DIR"
 echo "Done."
 
