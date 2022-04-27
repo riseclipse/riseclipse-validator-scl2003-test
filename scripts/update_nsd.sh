0bbf2d5fbcb5970f9340493d151273d532c76380
Dominique Marcadet <dominique.marcadet@centralesupelec.fr>
2022-04-27 11:27:06 +0200

    Update update_nsd.sh
    no default url, optional cleaning

--- scripts/update_nsd.sh
+++ scripts/update_nsd.sh
@@ -5,40 +5,39 @@ SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
 . "$SCRIPTS_DIR/utils.sh"
 
 usage() {
-    printf "Usage: $0 [OPTION...]\n\n"
+    printf "Usage: $0 [OPTION...] URL\n\n"
     printf "Updates NSD validation files from the given URL by downloading them to the desired target directory.\n"
-    printf "Files will only be overwritten if they were updated since the last download.\n\n"
     printf "Options:\n"
     printf -- "-h --help        \tShow this help message\n"
-    printf -- "-u --url         \tThe URL to download NSD validation files from.\n"
-    printf                 "\t\t\tDefault is '$DEFAULT_NSD_URL'.\n"
+    printf -- "-c --clean       \tRemove unneeded .xml and .xsd files\n"
     printf -- "-o --out-dir     \tThe directory in which you want to store NSD files.\n"
     printf                 "\t\t\tDefault is '<ROOT>/nsd' where <ROOT> is the root of the testing repository.\n"
 }
 
-OPTS=$(getopt -o hu:o: -l help,url:,out-dir: -- "$@")
+OPTS=$(getopt -o hco: -l help,clean,out-dir: -- "$@")
 
 if [ $? -ne 0 ]; then
-    echo "Options parsing failed, exiting..."
+    usage
     exit 1
 fi
 
 eval set -- "$OPTS"
+
+CLEAN=0
 
 while true; do
     case "$1" in
         -h | --help)
             usage
             exit 0;;
-        -u | --url)
-            arg_required "$1" "$2"
-            NSD_URL="$2"
-            shift;;
+        -c | --clean)
+            CLEAN=1
+            ;;
         -o | --out-dir)
             arg_required "$1" "$2"
             NSD_OUTPUT_DIR="$2"
             shift;;
-        --)
+        --) 
             shift
             break;;
         *)
@@ -50,12 +49,25 @@ while true; do
 done
 
 if [ -n "$1" ]; then
-    echo "Invalid argument: $1" 1>&2
+    NSD_URL=$1
+else
+    echo "missing URL" 1>&2
+    usage
     exit 1
 fi
 
-if [ -z "$NSD_URL" ]; then
-    NSD_URL="$DEFAULT_NSD_URL"
+if [ -n "$2" ]; then
+    echo "Invalid argument: $2" 1>&2
+    usage
+    exit 1
+fi
+
+FILENAME=$(basename $NSD_URL)
+EXTENSION=${FILENAME##*.}
+
+if [ $EXTENSION != "zip" -a $EXTENSION != "ZIP" ]; then
+    echo "not a zip file: $FILENAME" 1>&2
+    exit 1
 fi
 
 if [ -z "$NSD_OUTPUT_DIR" ]; then
@@ -63,12 +75,16 @@ if [ -z "$NSD_OUTPUT_DIR" ]; then
 fi
 
 mkdir -p "$NSD_OUTPUT_DIR"
-NSD_ZIP="$NSD_OUTPUT_DIR/nsd.zip"
 
 echo "Fetching NSD files from $NSD_URL..."
-wget -q -O "$NSD_ZIP" "$NSD_URL"
-unzip -q -u "$NSD_ZIP" -d "$NSD_OUTPUT_DIR"
-rm "$NSD_ZIP"
-echo "Done."
+wget -q "$NSD_URL"
+echo "Unzipping NSD files in $NSD_OUTPUT_DIR"
+unzip -q -u $FILENAME -d "$NSD_OUTPUT_DIR"
+rm $FILENAME
+
+if [ "$CLEAN" -eq 1 ]; then
+    echo "Cleaning..."
+    rm "$NSD_OUTPUT_DIR"/*.xml "$NSD_OUTPUT_DIR"/*.xsd
+fi
 
 exit 0
