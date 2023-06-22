2b10bc3a04d83c893e90165f41534a880b834877
Dominique Marcadet <dominique.marcadet@centralesupelec.fr>
2023-06-22 10:05:27 +0200

    remove stop on non zero exit code
    Validator now returns non-zero if an error is detected. oclContraintsTests are expected errors

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -14,12 +14,13 @@ FAILED_TESTS=""
 echo -n "Running tests... "
 
 for filepath in "${SCL_PATHS[@]}"; do
+    echo "Processing $filepath"
     OUTPUT=$(java -jar "$JAR_PATH" $JAR_OPTS "$filepath" $OCL_PATHS $NSD_PATHS)
 
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-        exit $exit_code
-    fi
+#    exit_code=$?
+#    if [ $exit_code -ne 0 ]; then
+#        exit $exit_code
+#    fi
 
     ORDERED_OUTPUT=$(printf "$OUTPUT" |sort)
 
